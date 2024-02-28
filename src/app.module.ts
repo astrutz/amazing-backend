7a1ef1bca9b20910851a3451cac6ff4ed0b7219b
karimcode379 <karimcode379@gmail.com>
2024-02-28 14:33:01 +0100

    add support for process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MarkerModule } from './marker/marker.module';
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [MarkerModule, MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASWORD}@cluster0.ks2c83d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)],
+  imports: [
+    MarkerModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.ks2c83d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
