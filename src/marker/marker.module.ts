8d45d22eab9a1eedc410246438a0b67191a5ce4a
Niklas Pooschke <n.pooschke@babiel.com>
2024-03-14 15:13:18 +0100

    readd the update and deleteMarker function bc alex wanted to delete our shit

--- src/marker/marker.module.ts
+++ src/marker/marker.module.ts
@@ -3,11 +3,12 @@ import { MarkersService } from './markers.service';
 import { MarkerController } from './marker.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Marker, MarkerSchema } from './schemas/marker.schema';
+import { GeheimmarkerController } from './geheimMarker.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Marker.name, schema: MarkerSchema }])],
   providers: [MarkersService],
-  controllers: [MarkerController],
+  controllers: [MarkerController, GeheimmarkerController],
 })
 export class MarkerModule {
 }
--- src/marker/markers.service.ts
+++ src/marker/markers.service.ts
@@ -3,6 +3,7 @@ import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Connection, Model } from 'mongoose';
 import { CreateMarkerDto } from './dto/CreateMarker.dto';
 import { Marker } from './schemas/marker.schema';
+import { UpdateMarkerDto } from './dto/UpdateMarker.dto';
 
 @Injectable()
 export class MarkersService {
@@ -14,6 +15,14 @@ export class MarkersService {
   async createMarker(createMarkerDto: CreateMarkerDto): Promise<Marker> {
     const newMarker = new this.markerModel(createMarkerDto);
     return newMarker.save();
+  }
+  
+  async updateMarker(markerId: string, updateMarkerDto: UpdateMarkerDto): Promise<Marker> {
+    return this.markerModel.findByIdAndUpdate(markerId, updateMarkerDto, {new: true});
+  }
+
+  async deleteMarker(markerId: string): Promise<Marker> {
+   return this.markerModel.findByIdAndDelete(markerId);
   }
 
   async getMarkers(): Promise<Marker[]> {
